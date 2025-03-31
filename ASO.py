db09e5e0ace5be7fb024667e22ce15ae72801fc2
Fabrizio <fabrizio.apparicio@gmail.com>
2025-03-31 11:23:11 -0300

    Leve modificação no código

--- ASO.py
+++ ASO.py
@@ -3,6 +3,8 @@ import smtplib
 import datetime
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from io import BytesIO
 
 # Configurações do e-mail
 SMTP_SERVER = "smtp.office365.com"
@@ -11,15 +13,45 @@ EMAIL_SENDER = "fabrizio.apparicio@decisionbr.com.br"
 EMAIL_PASSWORD = "kbrhrwbpmrbzjfvy"
 
 def enviar_email(destinatario, nome):
-    """Envia um e-mail de notificação."""
+    """Envia um e-mail de notificação com assinatura HTML."""
     assunto = "Lembrete: ASO Anual"
-    mensagem = f"Olá {nome},\n\nEste é um lembrete de que você precisa realizar seu exame ASO anual.\n\nAtenciosamente,\nEquipe de Saúde Ocupacional"
 
+    # Assinatura HTML com tabela e imagem
+    assinatura_html = """
+    <table>
+        <tbody><tr>
+            <td>
+            <a href="https://www.decisionbr.com/">
+                <img src="https://drive.google.com/uc?id=1WwhrTiAc83EOzGETQd-NNMr9VGjS5wyx" alt="image">
+            </a>
+            </td>
+            <td style="margin: 20px; font-size: 12px; color: grey; font-family: Verdana, sans-serif;">
+            <p style="margin-bottom: -20px;">________________________________________________________________</p>
+                <p><p style="margin-bottom: 10px; color: #4D2DD0; font-size: 14px;">Fabrizio | Desenvolvimento Humano e Organizacional</p>
+                Decision | Unidade São Paulo<br> 
+                Telefone: (19) 3252-2838 | (11) 98280-2722<br>
+                <a style="color: blue;" href="https://www.decisionbr.com/">www.decisionbr.com.br</a>
+            </p>
+            </td>
+        </tr></tbody>
+    </table>
+
+    """
+
+    mensagem = f"Olá {nome},<br><br>Este é um lembrete de que você precisa realizar seu exame ASO anual.<br><br>{assinatura_html}"
+
+    # Criando a mensagem com MIME
     msg = MIMEMultipart()
     msg["From"] = EMAIL_SENDER
     msg["To"] = destinatario
     msg["Subject"] = assunto
-    msg.attach(MIMEText(mensagem, "plain"))
+    msg.attach(MIMEText(mensagem, "html"))
+
+    # # Adicionando imagem à assinatura
+    # with open("logo_empresa.png", "rb") as img_file:
+    #     img = MIMEImage(img_file.read())
+    #     img.add_header("Content-ID", "<logo>")
+    #     msg.attach(img)
 
     try:
         with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
